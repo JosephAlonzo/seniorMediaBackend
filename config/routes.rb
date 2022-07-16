c157d947afcbd0efb2b950b89cd96d56f9b2470c
josephAlonzo <josep_alonzo@icloud.com>
2022-07-16 12:30:37 +0200

    add user endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
-Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }
+Rails.application.routes.draw do   
+  devise_for :users, defaults: { format: :json },
+  controllers: {
+    sessions: 'users/sessions'
+  }
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
